ea4f520dbed8ad0102215f5483439d2b88405f56
Leonardo David <leondavidtb@gmail.com>
2021-09-02 10:39:33 -0300

    Projeto finalizado - Teste de integração simulando API; - simulando evento de click; - Teste unitário de função; - Teste de snapshot;

--- src/App.test.js
+++ src/App.test.js
@@ -1,60 +1,53 @@
-import React from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 
-import App, { calcularNovoSaldo } from "./App";
+import App, { calcularNovoSaldo } from './app';
 
-describe("Componente principal", () => {
-  describe("Quando abro o app", () => {
-    it("Nome do banco é exibido", () => {
+describe('Componente principal', () => {
+  describe('Quando eu abro o app do banco', () => {
+    test('o nome é exibido', () => {
       render(<App />);
-      expect(screen.getByText("ByteBank")).toBeInTheDocument();
-    });
-
-    it("Saldo é exibido", () => {
+  
+      expect(screen.getByText('ByteBank')).toBeInTheDocument();
+    })
+    it('o saldo é exibido', () => {
       render(<App />);
-      expect(screen.getByText("Saldo:")).toBeInTheDocument();
-    });
-
-    it("Botão de transação é exibido", () => {
+    
+      expect(screen.getByText('Saldo:')).toBeInTheDocument();
+    })
+    it('o botão de realizar transação é exibido', () => {
       render(<App />);
-      expect(screen.getByText("Realizar operação")).toBeInTheDocument();
-    });
-  });
-
-  describe("Quando realizo uma transação", () => {
-    it("do tipo saque, o valor diminui", () => {
+  
+      expect(screen.getByText('Realizar operação')).toBeInTheDocument();
+    })
+  })
+  describe('Quando eu realizo uma transação', () => {
+    it('que é um saque, o valor vai diminuir', () => {
       const valores = {
-        transacao: "saque",
-        valor: 50,
-      };
-      const novoSaldo = calcularNovoSaldo(valores, 150);
+        transacao: 'saque',
+        valor: 50
+      }
+
+      const novoSaldo = calcularNovoSaldo(valores, 150)
+
       expect(novoSaldo).toBe(100);
     });
-
-    it("do tipo deposito, o valor aumenta", () => {
-      const valores = {
-        transacao: "deposito",
-        valor: 50,
-      };
-      const novoSaldo = calcularNovoSaldo(valores, 50);
-      expect(novoSaldo).toBe(100);
-    });
-
-    it("Do tipo saque, a transação deve ser realizada", () => {
+    it('que é um saque, a transação deve ser realizada', () => {
       render(<App />);
 
-      const saldo = screen.getByText("R$ 1000");
-      const transacao = screen.getByLabelText("Saque");
-      const valor = screen.getByTestId("valor");
-      const botaoTransacao = screen.getByText("Realizar operação");
+      const saldo = screen.getByText('R$ 1000');
+      const transacao =  screen.getByLabelText('Saque');
+      const valor = screen.getByTestId('valor');
+      const botaoTransacao = screen.getByText('Realizar operação');
 
-      expect(saldo.textContent).toBe("R$ 1000");
+      expect(saldo.textContent).toBe('R$ 1000')
 
-      fireEvent.click(transacao, { targe: { value: "saque" } });
-      fireEvent.change(valor, { target: { value: 10 } });
+      fireEvent.click(transacao, { target: { value: 'saque'}});
+      fireEvent.change(valor, { target: { value:  10 }});
       fireEvent.click(botaoTransacao);
 
-      expect(saldo.textContent).toBe("R$ 990");
-    });
-  });
-});
+      expect(saldo.textContent).toBe('R$ 990');
+    })
+  })
+})
+
--- src/transacoes/Transacao.test.js
+++ src/transacoes/Transacao.test.js
@@ -1,14 +1,15 @@
-import React from "react";
-import Transacao from "./Transacao";
-import { render } from "@testing-library/react";
+import React from 'react';
+import { render } from '@testing-library/react';
+import Transacao from './Transacao';
 
-describe("Componente de transação do extrato", () => {
-  it("O snapshot do componente deve permanecer sempre o mesmo", () => {
-    const { container } = render(
-      <Transacao data="01/09/2021" tipo="saque" valor="20.00" />
-    );
+describe('Componente de transação do extrato', () => {
+  it('O snapshot do component deve permanecer sempre o mesmo', () => {
+    const { container } = render(<Transacao
+      data="08/09/2020"
+      tipo="saque"
+      valor="20.00"
+    />)
 
     expect(container.firstChild).toMatchSnapshot();
-  });
-
-});
+  })
+})
